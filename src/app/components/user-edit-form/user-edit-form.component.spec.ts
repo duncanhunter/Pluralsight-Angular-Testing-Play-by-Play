9aac8f3f6e4c986530645eec91faf3f73c4290e0
duncanhunter <duncan@duncanhunter.com.au>
2018-02-13 22:55:09 +1100

    passing tests

--- src/app/components/user-edit-form/user-edit-form.component.spec.ts
+++ src/app/components/user-edit-form/user-edit-form.component.spec.ts
@@ -1,25 +1,27 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { UserEditFormComponent } from './user-edit-form.component';
+import { UserEditFormComponent } from './user-edit-form.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-// describe('UserEditFormComponent', () => {
-//   let component: UserEditFormComponent;
-//   let fixture: ComponentFixture<UserEditFormComponent>;
+describe('UserEditFormComponent', () => {
+  let component: UserEditFormComponent;
+  let fixture: ComponentFixture<UserEditFormComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ UserEditFormComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UserEditFormComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(UserEditFormComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserEditFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
